9eded60bd1ecf3da5d2d52ac563b3e8552fb3de2
ulzcq <syj9913@ynu.ac.kr>
2022-06-29 18:05:16 +0900

    Feat: 기본 객체들
    기본객체, 편의객체
    
    Issues: #4

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -51,6 +51,18 @@ public class BasicController {
 
     }
 
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data){
+            return "Hello" + data;
+        }
+    }
 
     @Data
     static class User{
