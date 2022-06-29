f606d9bf00d8250f04d294e106640f39e127ce72
ulzcq <syj9913@ynu.ac.kr>
2022-06-29 20:05:20 +0900

    Feat: 속성 값 지정
    th: 로 기존 속성값을 대체 및 생성
    th:checked는 HTML의 불편한 점 개선함
    
    Issues: #9

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -78,6 +78,11 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(Model model){
+        return "basic/attribute";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data){
